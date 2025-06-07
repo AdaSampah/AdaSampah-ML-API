5b8025d2dc5b8c166f5de330fdedf39251d0ee1a
chozam <choirunnizam175@gmail.com>
2025-06-07 15:40:05 +0700

    update model2 (add new class), update detail.json

--- handler/handler.js
+++ handler/handler.js
@@ -4,16 +4,17 @@ const fs = require('fs');
 
 let modelCleanDirty;
 let modelEducation;
+let label;
 
 async function loadModel() {
   try {
     modelCleanDirty = await tf.loadGraphModel(
-      "file://" + __dirname + "/../model/cleanDirty/model.json"
+      "file://" + __dirname + "/../model/cleanDirty/main/model.json"
     );
     console.log("Graph model Bersih Kotor loaded from local file");
 
     modelEducation = await tf.loadGraphModel(
-      "file://" + __dirname + "/../model/edukasi/model.json"
+      "file://" + __dirname + "/../model/edukasi/main/model.json"
     );
     console.log("Graph model Edukasi loaded from local file");
   } catch (err) {
@@ -30,20 +31,26 @@ async function predictCleanDirtyHandler(request, h) {
         .response({ error: "Image file is required (field name: image)" })
         .code(400);
     }
-    const readCleanDirtyLabel = fs.readFileSync('./model/cleanDirty/label.txt', 'utf8');
-    const labelCleanDirty = readCleanDirtyLabel.split('\n').map(line => line.trim());;
     const buffer = file._data || file;
     let imageTensor = tf.node.decodeImage(buffer, 3);
     imageTensor = tf.image.resizeBilinear(imageTensor, [224, 224]);
     imageTensor = imageTensor.div(255.0);
     const inputTensor = imageTensor.expandDims(0);
     const prediction = modelCleanDirty.predict(inputTensor);
-    const indexLabel = tf.argMax(prediction, 1).dataSync();
-    const probability = prediction.dataSync()[indexLabel];
-    
+    const score = prediction.dataSync()[0];
+    if (score > 0.7){
+      label = 'kotor';
+    }
+    else if (score < 0.4){
+      label = 'bersih';
+    }
+    else {
+      label = 'Tidak dapat diidentifikasi'
+    }
+
     return { 
-        predictedLabel: labelCleanDirty[indexLabel],
-        scores: probability
+        predictedLabel: label,
+        scores: score
     };
   } catch (err) {
     return h.response({ error: err.message }).code(400);
@@ -59,9 +66,11 @@ async function predictEdukasiHandler(request, h) {
         .code(400);
     }
     const readEdukasiLabel = fs.readFileSync('./model/edukasi/label.txt', 'utf8');
-    const labelEdukasi = readEdukasiLabel.split('\n').map(line => line.trim());;
+    const labelEdukasi = readEdukasiLabel.split('\n').map(line => line.trim());
     const readDetail = fs.readFileSync('./model/edukasi/detail.json', 'utf8');
     const jsonDetail = JSON.parse(readDetail);
+    const readLabelIndo = fs.readFileSync('./model/edukasi/label_indo.json', 'utf8');
+    const jsonLabelIndo = JSON.parse(readLabelIndo);
     const buffer = file._data || file;
     let imageTensor = tf.node.decodeImage(buffer, 3);
     imageTensor = tf.image.resizeBilinear(imageTensor, [256, 256]);
@@ -69,10 +78,11 @@ async function predictEdukasiHandler(request, h) {
     const inputTensor = imageTensor.expandDims(0);
     const prediction = modelEducation.predict(inputTensor);
     const indexLabel = tf.argMax(prediction, 1).dataSync()[0];
+    const label = labelEdukasi[indexLabel]
     const probability = prediction.dataSync()[indexLabel];
     
     return { 
-        predictedLabel: labelEdukasi[indexLabel],
+        predictedLabel: jsonLabelIndo[label],
         scores: probability,
         detail: jsonDetail[labelEdukasi[indexLabel]]
      };
